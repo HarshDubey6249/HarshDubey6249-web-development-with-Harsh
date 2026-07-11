d23d42a1892697767f5b392275d8614e5d9e7354
Harsh Dubey6249 <harshdubey9161@gmail.com>
2026-07-11 19:44:17 +0530

    lecture 4 complete  data type

---
+++ Javascript wit harsh/Lecture 4/dataType.js
@@ -0,0 +1,18 @@
+//code readibility
+
+console.log(2
+    +
+    2
+)   //we not write this
+//alert(2+2)  //  It is only work on it crome
+
+//=-----------------DATA TYPE-----------------------------
+
+//number
+//bigint
+//int 
+//string
+//boolean
+// null => standalone value
+//undefined 
+//symbol =>unique
