819db54e9cbaa357d2a820dbc2d83370e032e5e2
Harsh Dubey6249 <harshdubey9161@gmail.com>
2026-07-13 21:46:10 +0530

    add data summery in

---
+++ Javascript wit harsh/Lecture 8/data type.js
@@ -0,0 +1,30 @@
+const id=12344;
+console.log(typeof(id))
+
+const user_name="harsh";
+console.log(typeof(user_name))
+
+let isLoggedIn=false
+console.log(typeof(isLoggedIn))
+
+let functio=()=>{
+    console.log("harsh");
+}
+
+console.log(typeof(functio))
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+console.log( typeof myObj);
+
+
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
