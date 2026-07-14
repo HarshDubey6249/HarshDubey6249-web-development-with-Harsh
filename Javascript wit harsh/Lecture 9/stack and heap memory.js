3e72ffd6a0287a39828afa75c9e9460643ad2133
Harsh Dubey6249 <harshdubey9161@gmail.com>
2026-07-14 13:30:43 +0530

    stack and heap memory lecture 9 done

---
+++ Javascript wit harsh/Lecture 9/stack and heap memory.js
@@ -0,0 +1,29 @@
+//  stack (Primitive type)
+// Heap (NON Primitive type)
+
+//-------------------------------------------------------
+
+let userName="harsh"
+let userNameTwo=userName;    //copy
+
+userNameTwo="trisha";
+
+// console.log(userName);
+// console.log(userNameTwo);
+
+//---------------------------------------------------------
+
+
+let youTubeObj={
+
+    eamil:"harsh@jbl"
+}
+
+
+let youTubeObj2=youTubeObj;
+
+youTubeObj2.eamil="harshDubey."
+
+console.log(youTubeObj.eamil)
+console.log(youTubeObj2.eamil);
+
