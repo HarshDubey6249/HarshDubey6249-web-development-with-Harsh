68168592bc900cb1ce2f6af069ccd90ae91464a2
Harsh Dubey6249 <harshdubey9161@gmail.com>
2026-07-14 15:17:01 +0530

    lecture 10 string

---
+++ Javascript wit harsh/lecture 10/string.js
@@ -0,0 +1,15 @@
+let villageName="                 Lalpur                        ";
+let postName="                 sotipur                    ";
+let myName="harsh"
+
+
+let finalResult=` MY name is ${myName.trim() } from ${villageName.trim().toUpperCase()} post ${postName.trim()} `;
+
+console.log(finalResult);
+
+
+console.log(myName.slice(0,3));
+
+//replace(pattern, replacement)
+console.log(myName.replace("h","hh"))
+
