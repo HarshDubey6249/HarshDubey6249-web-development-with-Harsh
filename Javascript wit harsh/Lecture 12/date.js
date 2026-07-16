dc708e90806057eb87d81718ccafbb675f38b05f
Harsh Dubey6249 <harshdubey9161@gmail.com>
2026-07-16 15:57:29 +0530

    date

---
+++ Javascript wit harsh/Lecture 12/date.js
@@ -0,0 +1,19 @@
+let currentDate= new Date();
+
+// console.log(currentDate)
+//console.log(currentDate.toString())
+//console.log(currentDate.toLocaleString())
+//console.log(currentDate.toLocaleDateString())
+
+let myNewDAte=new Date("11-02-2023")
+console.log(myNewDAte.getMonth());
+
+
+// let currentDate2= Date.now()
+// //console.log(currentDate2);
+
+// if (1784197142831 === currentDate2) {
+//     console.log("True happy");
+// } else {
+//     console.log(false);
+// }
