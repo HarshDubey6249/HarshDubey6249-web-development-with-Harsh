ebf13a9527ad97e0ebd5605ade046ce4ad7b3367
Harsh Dubey6249 <harshdubey9161@gmail.com>
2026-07-22 12:27:50 +0530

    json

---
+++ Javascript wit harsh/Lecture 15/json.js
@@ -0,0 +1,17 @@
+const course={
+
+    courseName:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+
+const {courseInstructor, price:p}=course;
+
+console.log(courseInstructor);
+console.log(p);
+
+// {
+//     "name":"harsh",
+//     "courseName":"js name",
+
+// }
