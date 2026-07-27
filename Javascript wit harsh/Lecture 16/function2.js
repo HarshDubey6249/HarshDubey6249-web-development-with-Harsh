28d35b5bbd1088ec34039d407c59799be2f87418
Harsh Dubey6249 <harshdubey9161@gmail.com>
2026-07-27 23:58:53 +0530

    function part complete

---
+++ Javascript wit harsh/Lecture 16/function2.js
@@ -0,0 +1,48 @@
+
+
+
+// // function cartWithoutRest(val){
+// //     return val;
+// // }
+
+// // console.log(cartWithoutRest(200,3000,400));
+
+// // function cartWithRest(...val){
+// //     return val;
+// // }
+
+// // console.log(cartWithRest(200,3000,400));
+
+
+// ///object
+
+// const User={
+
+//     username:"harsh",
+//     role:"CEO"
+
+// }
+
+// function obj(anyObj){
+
+//     console.log(`hi i am ${anyObj.username} ${anyObj.role} of this company`)
+// }
+
+// obj(
+//     {
+
+//     username:"harsh dubey",
+//     role:"CEO"
+
+// }
+// )
+
+// obj(User);
+
+// const arr=[1,2,3,4,5];
+
+// function arrFun(arr){
+//     console.log(arr[1]);
+// }
+
+// arrFun(arr)
