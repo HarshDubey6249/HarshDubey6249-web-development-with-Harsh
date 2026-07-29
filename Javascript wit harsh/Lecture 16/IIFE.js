c58db0266062dffde63af49696bdd8cf639b4c9b
Harsh Dubey6249 <harshdubey9161@gmail.com>
2026-07-29 16:20:58 +0530

    learn about named iife

---
+++ Javascript wit harsh/Lecture 16/IIFE.js
@@ -0,0 +1,14 @@
+
+
+// Immediately Invoked function expression
+
+(function Greet(){
+
+    //name IIfe
+    console.log("HI welocome to jungle");
+})();
+
+((val)=>{
+  console.log(`Good evening sir Your bill is ${val} pound sir `);
+  
+})(23);
