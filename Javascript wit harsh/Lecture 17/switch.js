8f38294552c5ecb8c2e96575fe9941c750526b45
Harsh Dubey6249 <harshdubey9161@gmail.com>
2026-08-05 21:18:06 +0530

    Implement switch case for month selection

---
+++ Javascript wit harsh/Lecture 17/switch.js
@@ -0,0 +1,42 @@
+// ==============================
+// Switch Statement Syntax
+// ==============================
+
+// switch (key) {
+//     case value:
+//         // Code to execute
+//         break;
+
+//     default:
+//         // Default code
+//         break;
+// }
+
+
+// ==============================
+// Switch Statement Example
+// ==============================
+
+const month = "march";
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+
+    case "feb":
+        console.log("February");
+        break;
+
+    case "march":
+        console.log("March");
+        break;
+
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default case matched");
+        break;
+}
